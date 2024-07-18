baa235c0568ac62dcb84b3e2369f19c4ab434a70
badtux <badtux@users.noreply.github.com>
2024-07-18 15:08:08 +0530

    Update timer.dart

--- d3/timer.dart
+++ d3/timer.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Custom Widget with Animation Timer'),
+        ),
+        body: Center(
+          child: AnimatedWidget(),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedWidget extends StatefulWidget {
+  @override
+  _AnimatedWidgetState createState() => _AnimatedWidgetState();
+}
+
+class _AnimatedWidgetState extends State<AnimatedWidget> {
+  double _size = 100.0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _size = _size == 100.0 ? 200.0 : 100.0;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(seconds: 1),
+      width: _size,
+      height: _size,
+      color: Colors.blue,
+      child: Center(
+        child: Text(
+          'Animate',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
