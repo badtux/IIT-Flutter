0e1bde0c8482ccff79573fddc4f8b185ad15773e
badtux <badtux@users.noreply.github.com>
2024-07-18 16:19:16 +0530

    Update fire.dart

--- d3/fire.dart
+++ d3/fire.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Button Clicks',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  void _incrementCounter() {
+    _firestore.collection('button_clicks').add({
+      'timestamp': Timestamp.now(),
+    }).then((value) {
+      print('Button click recorded!');
+    }).catchError((error) {
+      print('Failed to record button click: $error');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Button Clicks'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Click the button to record a click!',
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Text('Record Click'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
