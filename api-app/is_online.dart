ef026d82e38f656e754679baa887b868188585d0
badtux <badtux@users.noreply.github.com>
2024-07-23 14:31:29 +0530

    Update is_online.dart

--- api-app/is_online.dart
+++ api-app/is_online.dart
@@ -1,1 +1,67 @@
 
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'connectivity_service.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Internet Connectivity Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ConnectivityService _connectivityService = ConnectivityService();
+
+  @override
+  void initState() {
+    super.initState();
+    _connectivityService.connectivityStream.listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.none) {
+        _showToast('You are offline');
+      } else {
+        _showToast('You are online');
+      }
+    });
+  }
+
+  void _showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Internet Connectivity Demo'),
+      ),
+      body: Center(
+        child: Text('Check your internet connection status.'),
+      ),
+    );
+  }
+}
