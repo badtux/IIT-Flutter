2be0f3c226ffe8e501d9c32e4a95e6b79079742a
badtux <badtux@users.noreply.github.com>
2024-07-23 12:48:13 +0530

    Update sqlite.dart

--- api-app/sqlite.dart
+++ api-app/sqlite.dart
@@ -1,1 +1,90 @@
 
+import 'package:flutter/material.dart';
+import 'database_helper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SQLite Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _controller = TextEditingController();
+  final _databaseHelper = DatabaseHelper();
+  List<Map<String, dynamic>> _data = [];
+
+  void _addData() async {
+    if (_controller.text.isNotEmpty) {
+      await _databaseHelper.insertData(_controller.text);
+      _controller.clear();
+      _loadData();
+    }
+  }
+
+  void _loadData() async {
+    final data = await _databaseHelper.fetchData();
+    setState(() {
+      _data = data;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SQLite Demo'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter some text',
+              ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: _addData,
+            child: Text('Add Data'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _data.length,
+              itemBuilder: (context, index) {
+                final item = _data[index];
+                return ListTile(
+                  title: Text(item['value']),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
