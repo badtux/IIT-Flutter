e7878cfcea98bbf25a56200bff70053883d11f9a
badtux <badtux@users.noreply.github.com>
2024-07-25 14:34:33 +0530

    Update will_pop.dart

--- nav/will_pop.dart
+++ nav/will_pop.dart
@@ -1,1 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return await _showExitConfirmationDialog(context) ?? false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Second Screen'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () async {
+              bool? confirm = await _showExitConfirmationDialog(context);
+              if (confirm == true) {
+                Navigator.pop(context);
+              }
+            },
+            child: Text('Go back to First Screen'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool?> _showExitConfirmationDialog(BuildContext context) {
+    return showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm Exit'),
+          content: Text('Do you really want to go back?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
 
